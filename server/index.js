9a53a50b9aac99ebb578d77acf0180beb07e171d
VinayakGaikwad101 <vinaayakgaikwad@gmail.com>
2025-06-30 01:12:12 +0530

    Feat: add get transactions controller

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   return res.send({ message: "Server is running", success: true });
 });
 
-app.post("/api/transaction", async (req, res) => {
+app.post("/api/transactions", async (req, res) => {
   try {
     const { user_id, title, amount, category } = req.body;
 
@@ -42,7 +42,33 @@ app.post("/api/transaction", async (req, res) => {
       transaction,
     });
   } catch (error) {
-    console.error("Error in transaction controller: ", error);
+    console.error("Error in create transaction controller: ", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", success: false });
+  }
+});
+
+app.get("/api/transactions/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const transactions = await sql`
+    SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC`;
+
+    if (transactions.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No transactions found for user", success: false });
+    }
+
+    return res.status(200).json({
+      message: "User transactions found!",
+      success: true,
+      transactions,
+    });
+  } catch (error) {
+    console.error("Error in get transaction controller: ", error);
     return res
       .status(500)
       .json({ message: "Internal server error", success: false });
