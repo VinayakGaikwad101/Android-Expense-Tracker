8f4be310e66a0e9b97c2aecf8cf748890996e83a
VinayakGaikwad101 <vinaayakgaikwad@gmail.com>
2025-06-29 21:55:37 +0530

    Refactor: Added DB connection inside ttry

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,20 +2,20 @@
 import { neon } from "@neondatabase/serverless";
 import "dotenv/config";
 
-// constants
-const DATABASE_URL = process.env.DATABASE_URL;
-
-// creates a sql connection
-const sql = neon(DATABASE_URL);
-
 // initialize database
 export async function initDB() {
   try {
+    // constants
+    const DATABASE_URL = process.env.DATABASE_URL;
+
+    // creates a sql connection
+    const sql = neon(DATABASE_URL);
+
     await sql`CREATE TABLE IF NOT EXISTS transactions(
-        id SERIAL PRIMARY KEY,
-        user_id VARCHAR(255) NOT NULL,
+        id SERIAL PRIMARY KEY, -- primary key means incremented by 1, eg: 1, 2, 3, 4, ...
+        user_id VARCHAR(255) NOT NULL, -- variable character(string/text), of max length of 255 characters
         title VARCHAR(255) NOT NULL,
-        amount DECIMAL(10, 2) NOT NULL,
+        amount DECIMAL(10, 2) NOT NULL, -- 10 digits total, 2 digits after decimal point, eg: 12345678.90
         category VARCHAR(255) NOT NULL,
         created_at DATE NOT NULL DEFAULT CURRENT_DATE
       )`;
