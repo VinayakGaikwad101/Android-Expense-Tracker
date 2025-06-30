2a8d18077c22a6c2f26bfabe8f702bf5bd916e1f
VinayakGaikwad101 <vinaayakgaikwad@gmail.com>
2025-06-30 09:22:12 +0530

    Add: layout and colors

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,27 +1,9 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-import { Image } from "expo-image";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>
-        Edit app/index.tsx to edit this screen.
-      </Text>
-      <Link href={"/about"}>About Us</Link>
+    <View>
+      <Text>Edit app/index.tsx to edit this c8t9e.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "purple",
-  },
-  heading: {
-    color: "white",
-    fontSize: 40,
-  },
-});
