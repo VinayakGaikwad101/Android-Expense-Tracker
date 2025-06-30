a8589c0b45c333d6bc5da25ca7af598dc7eafdae
VinayakGaikwad101 <vinaayakgaikwad@gmail.com>
2025-06-30 09:47:34 +0530

    Feat: add clerk authentication

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,14 @@
-import { Stack } from "expo-router";
 import SafeScreen from "@/components/SafeScreen";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { Slot } from "expo-router";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
 export default function RootLayout() {
   return (
     <SafeScreen>
-      <Stack screenOptions={{ headerShown: false }} />
+      <ClerkProvider tokenCache={tokenCache}>
+        <Slot />
+      </ClerkProvider>
     </SafeScreen>
   );
 }
--- mobile/app/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View>
-      <Text>Edit app/index.tsx to edit this c8t9e.</Text>
-    </View>
-  );
-}
