3b7f15914c2aabee671b696379dbf33d47762472
VinayakGaikwad101 <vinaayakgaikwad@gmail.com>
2025-07-19 10:16:09 +0530

    Fix: configured the env variables

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -2,11 +2,15 @@ import SafeScreen from "@/components/SafeScreen";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { Slot } from "expo-router";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import config from "@/config/env";
 
 export default function RootLayout() {
   return (
     <SafeScreen>
-      <ClerkProvider tokenCache={tokenCache}>
+      <ClerkProvider 
+        tokenCache={tokenCache}
+        publishableKey={config.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      >
         <Slot />
       </ClerkProvider>
     </SafeScreen>
